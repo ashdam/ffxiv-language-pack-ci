b0c3b4cc95aadd0936a4d22fd512d47aeca797a4
ashdam <ashdam@users.noreply.github.com>
2026-09-12 20:29:57 +0200

    Remove the place-name review path exception

--- scripts/validate-pr.sh
+++ scripts/validate-pr.sh
@@ -33,8 +33,6 @@ while IFS= read -r -d '' status && IFS= read -r -d '' path; do
     allowed='AM'
   elif [[ $path =~ ^glossary/[^/]+\.json$ ]]; then
     allowed='M'
-  elif [[ $path == review/toponimos-decisiones.csv ]]; then
-    allowed='AM'
   elif [[ $path =~ ^layouts/[^/]+\.json$ ]]; then
     allowed='AMD'
   elif [[ $path =~ ^fonts/[^/]+\.(fdt|tex)$ ]]; then
